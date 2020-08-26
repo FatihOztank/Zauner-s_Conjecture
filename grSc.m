2bd2ab300244b97c4e22e3cf3581319fee76acf8
FatihOztank <55440245+FatihOztank@users.noreply.github.com>
2020-08-27 00:48:51 +0300

    Update grSc.m

--- grSc.m
+++ grSc.m
@@ -1,5 +1,5 @@
 function mat = grSc(vector1, vector2, angle)
-    mat = zeros(2,length(vector1)); % 5 vector sizesi olmalı ?
+    mat = zeros(2,length(vector1)); 
     u_1 = vector1/norm(vector1);
     y_2 = vector2 - dot(vector2,u_1)*u_1 + norm(cot(angle)*(vector2 - dot(vector2,u_1)*u_1))*u_1;
     mat(1,:) = u_1;
